0c139ea25bd04741d098a14af0a1b1e1b762a069
Nicolas Manjarres <nicolasmanjarresg@gmail.com>
2024-01-03 20:47:38 -0500

    se crea base de datos mongo en la nube y crea archivo de configuración de la conexión a mongo

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var database = require('./config/database');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -18,6 +20,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+//Conexión a Mongo
+
+database.mongoConnect();
+
+//Router
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
