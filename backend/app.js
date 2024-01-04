4f03252040a53b54b0fa79166724b14fbca5082f
Nicolas Manjarres <nicolasmanjarresg@gmail.com>
2024-01-03 21:37:44 -0500

    se crean carpetas de modelo y controlador, además se crea ruta para empleados, se hacen pruebas en postman con la tabla empleados y funciona correctamente

--- backend/app.js
+++ backend/app.js
@@ -8,12 +8,9 @@ var database = require('./config/database');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var empleadosRouter = require('./routes/empleados.router');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -30,6 +27,7 @@ database.mongoConnect();
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/empleados', empleadosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
