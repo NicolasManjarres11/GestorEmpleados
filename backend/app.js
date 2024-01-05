2114ce54bacd30b0933ddf077aef5f7d00b461fb
Nicolas Manjarres <nicolasmanjarresg@gmail.com>
2024-01-04 22:07:45 -0500

    se crea modelo de usuario para datos del usuario, controlador de usuario para login, donde se agregan funciones de hasheo de contraseña y se comprueba que haya un usuario en la base de datos, se crea carpeta auth con archivo de autenticación principal donde se verifica y decodifica el token valido para la autenticación, en proceso de creación de rutas

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var database = require('./config/database');
+var auth = require('./auth/main_auth');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 var empleadosRouter = require('./routes/empleados.router');
 
 var app = express();
@@ -23,10 +23,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 database.mongoConnect();
 
+
+app.use(auth)
+
 //Router
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/empleados', empleadosRouter);
 
 // catch 404 and forward to error handler
