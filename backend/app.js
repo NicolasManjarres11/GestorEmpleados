256187ca1510e70a828dcff55cacc642494ab456
Nicolas Manjarres <nicolasmanjarresg@gmail.com>
2024-01-04 22:29:10 -0500

    aún está en proceso de creación de login, pendiente por verificar por´qué a pesar de colocar las credenciales, dice que no se tiene permisos

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ var auth = require('./auth/main_auth');
 
 
 var empleadosRouter = require('./routes/empleados.router');
+var usuariosRouter = require('./routes/usuarios.router')
 
 var app = express();
 
@@ -29,6 +30,8 @@ app.use(auth)
 //Router
 
 app.use('/empleados', empleadosRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
