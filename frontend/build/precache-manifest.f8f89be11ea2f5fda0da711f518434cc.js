ee6dc702dcf9f2a769c335e30f27ae8c9d212583
Nicolas Manjarres <nicolasmanjarresg@gmail.com>
2024-02-24 12:03:23 -0500

    se edita link del repositorio

---
+++ frontend/build/precache-manifest.f8f89be11ea2f5fda0da711f518434cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bb45ff56e24cf5f61beb4767ed14fd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd043462a1eb825427d4",
+    "url": "/static/css/2.016185da.chunk.css"
+  },
+  {
+    "revision": "2a1b417bbc31e8026aa4",
+    "url": "/static/css/main.8c60254a.chunk.css"
+  },
+  {
+    "revision": "fd043462a1eb825427d4",
+    "url": "/static/js/2.cd8f9159.chunk.js"
+  },
+  {
+    "revision": "254118eb303b9fadc6e5691572377f77",
+    "url": "/static/js/2.cd8f9159.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c65dc52484608f8c4b6",
+    "url": "/static/js/3.b1ae088f.chunk.js"
+  },
+  {
+    "revision": "2a1b417bbc31e8026aa4",
+    "url": "/static/js/main.2f979943.chunk.js"
+  },
+  {
+    "revision": "5e219d8fee070c41dd92",
+    "url": "/static/js/runtime-main.fdf094e9.js"
+  }
+]);
